6a12aca4225ca6101c95f855c2ef9af7f5f6d398
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2020-12-01 16:34:23 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
 <?php
 
-require_once "./framework/core/Framework.php";
+// require_once './router/Router.php';
 
-Framework::run();
+$url = isset($_SERVER['PATH_INFO']) ? explode('/', ltrim($_SERVER['PATH_INFO'], '/')) : [] ;
+echo var_dump($url);
+// $url = Router::getUrl();
+// echo $url;
+
+// if ($url != "/") {
+//     $controller_name = array_shift($url);
+//     $function_name = array_shift($url);
+//     $args = $url;
+//     call_user_func_array(array(new $controller_name, $function_name), $args);
+// }else {
+//     echo "404 not Found";
+// }
+
+// $Router = new Router;
+
+// $start = $Router->getArgumentStart($parameters);
