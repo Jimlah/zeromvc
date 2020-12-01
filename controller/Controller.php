6d0f20bd395756842958124aa654c8dbc4934c7b
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2020-12-01 16:34:14 +0100

    Create Controller.php

---
+++ controller/Controller.php
@@ -0,0 +1,15 @@
+<?php
+class Controller
+{
+    public function __construct()
+    {
+        echo 'Controller created';
+    }
+
+    function load_view($view, $args)
+    {
+        
+    }
+
+}
+
